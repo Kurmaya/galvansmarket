23f9277cf69bf7bcaa19b3b35e00ecdfc7a8fb13
kurmaya sid <kurmaya.sid@gmail.com>
2025-10-08 02:09:40 +0530

    mobile

---
+++ scripts/nav.js
@@ -0,0 +1,9 @@
+const closeBtn = document.querySelector('.close');
+const nav = document.querySelector('nav');
+const navLinks = nav.querySelector('.navbar');
+closeBtn.addEventListener('click',()=>{
+  nav.classList.toggle('active');
+  navLinks.classList.toggle('active');
+  closeBtn.classList.toggle('active');
+  document.querySelector('.store-details').classList.toggle('active');  
+})
--- scripts/side.js
+++ scripts/side.js
@@ -1,6 +1,7 @@
 const sideArrow = document.querySelector('.side-area img');
 const sideArea = document.querySelector('.side-area');
 
+
 sideArrow.addEventListener('click',()=>{
     sideArea.classList.toggle('active');
     sideArrow.classList.toggle('active');
@@ -10,4 +11,18 @@ sideArea.querySelectorAll('a').forEach(link=>{
     link.addEventListener('click',()=>{
         sideArea.classList.remove('active');
         sideArrow.classList.remove('active');
-    })})
+    })});
+
+    window.addEventListener('scroll',scrollFunction);
+function scrollFunction() {
+    console.log(window.pageYOffset)
+  if (window.pageYOffset > window.innerHeight-window.innerHeight*1 && window.pageYOffset < document.documentElement.scrollHeight-document.querySelector('footer').getBoundingClientRect().height+200) {
+    console.log('scrolled')
+    document.querySelector('.page').classList.add('active');
+      sideArea.classList.add('active');
+    } 
+    else {
+      document.querySelector('.side-area').classList.remove('active');
+      document.querySelector('.page').classList.remove('active');
+  }
+}
